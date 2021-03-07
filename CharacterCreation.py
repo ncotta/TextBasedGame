1686f1e489a85245cf23b50891073a620159bfac
Niklaas Cotta <nikcotta@gmail.com>
2021-03-06 21:17:26 -0800

    Bringing the character creation all together, a bit sloppy though...

--- CharacterCreation.py
+++ CharacterCreation.py
@@ -8,33 +8,83 @@ from Class import *
 
 
 class Character:
-    def __init__(self, charRace, charClass, stats, inventory=None):
+    def __init__(self, name, charRace, charClass, stats, inventory=None):
+        self.name = name
         self.myRace = charRace
         self.myClass = charClass
         self.attack = stats[0]
         self.defense = stats[1]
+        self.speed = stats[2]
         if inventory is None:
             inventory = []
 
         self.inventory = inventory
-        self.maxHP = 20
+        self.hp = 20
 
     def info(self):
         print('\n======= Character Info =======')
-        print(f"Race: {self.myRace.name}")
-        print(f"Class: {self.myClass.name}")
-        print(f"HP: {self.maxHP}")
+        print(f"{self.name}")
+        print(f"{self.myRace.name} {self.myClass.name}")
+        print(f"HP: {self.hp}")
         print(f"Attack: {self.attack}")
         print(f"Defense: {self.defense}")
 
-    def getInventory(self):
-        return self.inventory
+    @classmethod
+    def get_input(cls):
+        raceObj = None
+        race = input("What is your race?\n")
+
+        if race == "Lizard":
+            raceObj = Lizard()
+        elif race == "Werepus":
+            raceObj = Werepus()
+        elif race == "Monster-a":
+            raceObj = MonsterA()
+        else:
+            print("Unrecognizable")  # FIXME
+
+        classObj = None
+        myClass = input("What is your class?\n")
+
+        if myClass == "Brute":
+            classObj = Brute()
+        elif myClass == "MonkEY":
+            classObj = MonkEY()
+        elif myClass == "WitchDoctor":
+            classObj = WitchDoctor()
+        else:
+            print("Unrecognizable")  # FIXME
+
+        return cls(
+            input("What is your name?\n"),  # name
+            raceObj,
+            classObj,
+            [20, 10, 5]  # stats
+        )
+
+    """
+    def fight(self, enemy):
+        print(f"{self.name}   VS   {enemy.name}")
+
+        while self.hp > 0 and enemy.hp > 0:
+            print(f"Your HP: {self.hp}")
+            print(f"Enemy HP: {enemy.hp}")
+
+            if self.speed > enemy.speed:
+                pass
+    """
 
 
 if __name__ == '__main__':
+    """
     attack = 20
     defense = 10
+    speed = 5
+    inv = ["50 gp", "Magic sword"]
     testRace = Lizard()
     testClass = Brute()
-    characterObj = Character(testRace, testClass, [attack, defense], ["50 gp", "Magic sword"])
+    characterObj = Character("Bobby Schmurda", testRace, testClass, [attack, defense, speed], inv)
+    """
+    characterObj = Character.get_input()
     characterObj.info()
+    print(characterObj.inventory)
