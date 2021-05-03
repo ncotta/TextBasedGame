d0d09f885d86994eb1ee4693c75be4e1226e91b1
Niklaas Cotta <nikcotta@gmail.com>
2021-05-03 10:02:07 -0700

    minor documentation

--- CharacterCreation.py
+++ CharacterCreation.py
@@ -11,6 +11,15 @@ import time
 
 class Character:
     def __init__(self, name, charRace, charClass, stats, movesList, inventory=None):
+        """
+        Desc: Constructor for a Character object (self and enemy)
+        Input: name; a string that you want the character to be named
+               charRace; the race instance of the character
+               charClass; the class instance of the character
+               stats; list of attack, defense, speed numbers
+               movesList; a list of move objects
+               inventory; a list of items the character possesses, None by default
+        """
         self.name = name
         self.myRace = charRace
         self.myClass = charClass
@@ -25,6 +34,11 @@ class Character:
         self.hp = 20
 
     def info(self):
+        """
+        Desc: Prints out some general info of your character
+        Input: None
+        Output: None
+        """
         time.sleep(1)
         print('\n======= Character Info =======')
         time.sleep(0.5)
@@ -42,6 +56,11 @@ class Character:
 
     @classmethod
     def get_input(cls):
+        """
+        Desc: Class method, creates a character and fills in parameters with user input when called
+        Input: None
+        Output: Character() instance
+        """
         mySelection = Selection()
         raceObj = mySelection.race_select()
         classObj = mySelection.class_select()
@@ -57,6 +76,7 @@ class Character:
 
 
 if __name__ == '__main__':
+    # TODO: combine with Map.py main
     characterObj = Character.get_input()
     characterObj.info()
     print("")
