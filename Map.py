193ce3938478dd949b9bd3ea2f7dba4753cdb420
Niklaas Cotta <nikcotta@gmail.com>
2021-05-03 00:04:01 -0700

    started encounters code, added documentation

--- Map.py
+++ Map.py
@@ -5,6 +5,7 @@ Author: Niklaas Cotta
 
 import random
 import time
+from CharacterCreation import *
 
 
 # Map
@@ -65,6 +66,11 @@ class Map:
                     return col, row
 
     def placePlayer(self, coords):
+        """
+        Desc: Places player in map
+        Input: coords you want to place player
+        Output: None
+        """
         x, y = coords
         for row in range(self.size):
             for col in range(self.size):
@@ -78,6 +84,10 @@ class Map:
 # Tiles
 class Tile:
     def __init__(self):
+        """
+        Desc: Constructor for Tile class, gets x, y value
+        Input: None
+        """
         self.x = None
         self.y = None
 
@@ -85,7 +95,7 @@ class Tile:
         print("X: " + self.x + "Y: " + self.y)
 
 
-class Character(Tile):
+class Character(Tile):  # Player Tile
     def __init__(self):
         super().__init__()
         self.appearance = "<>"
@@ -93,7 +103,7 @@ class Character(Tile):
         self.encounterChance = 0
 
 
-class Grass(Tile):
+class Grass(Tile):  # Grasslands
     def __init__(self):
         super().__init__()
         self.appearance = "wW"
@@ -101,7 +111,7 @@ class Grass(Tile):
         self.encounterChance = 15
 
 
-class Mountain(Tile):
+class Mountain(Tile):  # Mountains
     def __init__(self):
         super().__init__()
         self.appearance = "//"
@@ -109,7 +119,7 @@ class Mountain(Tile):
         self.encounterChance = 25
 
 
-class Water(Tile):
+class Water(Tile):  # Water
     def __init__(self):
         super().__init__()
         self.appearance = "()"
@@ -119,28 +129,60 @@ class Water(Tile):
 
 # Encounters
 class Encounter:
-    def __init__(self, enemy):
-        self.enemy = enemy
+    def __init__(self, character, defStats=None):
+        if defStats is None:
+            defStats = [10, 10, 10]
+
+        self.character = character
+        self.stats = defStats
+        self.encounterMap = {1: Character("Dummy", Dummy(self.stats), None, self.stats, Dummy(self.stats).movesList),
+                             2: Character("Hulking Dummy", Dummy([12, 12, 12]), None, [12, 12, 12], Dummy([12, 12, 12]).movesList),
+                             3: Character("Blighted Rose", MonsterA(self.stats), None, self.stats, MonsterA(self.stats).movesList),
+                             4: Character("Hall Monitor", Lizard(self.stats), None, self.stats, Lizard(self.stats).movesList),
+                             5: Character("Cuddlefish", Werepus(self.stats), None, self.stats, Lizard(self.stats).movesList)}
+
+    def startBattle(self):
+        enemy = self.encounterMap[random.randint(1, 5)]
+        mapBattle = Fight(self.character, enemy).battle()
 
 
 # Map movement
 class Movement:
     def __init__(self):
+        """
+        Desc: Constructor for a movement, moveSpeed = how many tiles per turn
+        Input: None
+        """
         self.moveSpeed = 1  # Navigate one tile
 
     def swapTile2D(self, tile1, tile2, layout):
+        """
+        Desc: Swaps two tile objects in a 2D list
+        Input: tile1; first tile to swap
+               tile2; tile to swap with first
+               layout; the 2D list you want tiles swapped in
+        Output: tuple, returns new coords of tile1
+        """
         row1 = tile1.y
         col1 = tile1.x
 
         row2 = tile2.y
         col2 = tile2.x
+        # Swap actual layout in map
         layout[row1][col1], layout[row2][col2] = layout[row2][col2], layout[row1][col1]
-        row1, row2 = tile2.y, tile1.y
-        col1, col2 = tile2.x, tile1.x
+        row1, row2 = tile2.y, tile1.y  # swap y coord
+        col1, col2 = tile2.x, tile1.x  # swap x coord
 
         return col1, row1  # return new coords
 
     def getDest(self, direction, coords, layout):
+        """
+        Desc: Get destination of where tile is going
+        Input: direction; up (1), down (2), right (3), left (4)
+               coords; coords of tile you're moving
+               layout; 2d list you're using
+        Output: tuple, destination tile
+        """
         col, row = coords
         if direction == 1:  # North
             row -= self.moveSpeed
